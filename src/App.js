ca1d9fa409375ec6d875bd3e7b1ef2e94f98ddad
alihan <aliz1233773@gmail.com>
2023-06-25 15:45:04 +0600

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import PersonalArea from './pages/personal-area/PersonalArea';
+import MatchPage from './pages/match-page/MatchPage';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import ClosedMatch from './pages/closed-match/ClosedMatch';
+import ModalRent from './component/modal/modal-rent/ModalRent';
+export const MyContext = createContext()
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [modalRentOpen, setModalRentOpen] = useState(false)
+	const axiosInstance = axios.create({
+		baseURL: 'http://127.0.0.1:5000/api/',
+		headers: {
+		  "Authorization": `Bearer ${localStorage.getItem("token")}`
+		}
+	  });
+	const storedUserLogged = localStorage.getItem('userLogged') === 'true';
+	const [userLogged, setUserLogged] = useState(storedUserLogged);
+
+	return (
+		<MyContext.Provider value={{
+			userLogged,
+			setUserLogged,
+			axiosInstance,
+			setModalRentOpen,
+			modalRentOpen
+		}}>
+			<Routes>
+				<Route path='/' element={<Home></Home>}></Route>
+				<Route path='/login' element={<Login></Login>}></Route>
+				<Route path='/personal-area' element={<PersonalArea></PersonalArea>}></Route>
+				<Route path='/match-page' element={<MatchPage></MatchPage>}></Route>
+				<Route path='/closed-match' element={<ClosedMatch></ClosedMatch>}></Route>
+			</Routes>
+		</MyContext.Provider>
+	);
 }
 
 export default App;
